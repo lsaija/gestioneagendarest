3176ffeae2e7c0271bbf79b8706b44e3f8e093dc
lsaija <lollosaija@gmail.com>
2022-11-28 14:30:43 +0100

    creazione

---
+++ src/main/java/it/prova/gestioneagendarest/GestioneagendarestApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestioneagendarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneagendarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneagendarestApplication.class, args);
+	}
+
+}
